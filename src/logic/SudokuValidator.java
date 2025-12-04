ec9535de5c31193acc27959c8b133b46b2f23086
Mariam-Hussein05 <es.mariam.yamin2023@alexu.edu.eg>
2025-12-04 19:38:38 +0200

    Implement Sudoku validation logic

--- src/logic/SudokuValidator.java
+++ src/logic/SudokuValidator.java
@@ -6,8 +6,102 @@ package logic;
 
 /**
  *
- * @author Nour
+ * @author Mariam
  */
 public class SudokuValidator {
-    
+
+      public void validateSourceSolution(int[][] board){
+        if (!isBoardValid(board)) {
+            throw new IllegalArgumentException("The Sudoku solution is invalid.");
+        }
+        if (!isComplete(board)) {
+            throw new IllegalArgumentException("The Sudoku board is not complete.");
+        }
+        System.out.println("The Sudoku solution is valid.");
+        
+      }
+
+      private boolean isBoardValid(int[][] board) {
+        return isValidFormat(board) &&
+         validateAllRows(board) && validateAllcolumns(board) &&
+          validateAllBoxes(board);
+      }
+      private boolean isValidFormat(int[][] board) {//check if all values are between 0 and 9
+        for(int i=0;i<9;i++){
+            for(int j=0;j<9;j++){
+                int value = board[i][j];
+                if(value < 0 || value > 9){
+                    System.out.println("Invalid value found: " + value);
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    private boolean isComplete(int[][] board) {//check if there is any 0 in the board
+        for (int row = 0; row < 9; row++) {
+            for (int col = 0; col < 9; col++) {
+                if (board[row][col] == 0) {
+                    System.out.println("Board is not complete");
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private boolean validateAllRows(int[][] board) {
+        for (int row = 0; row < 9; row++) {
+            if (hasDuplicates(board[row])) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean validateAllcolumns(int[][] board){
+        for(int column = 0; column<9;column++){
+            int[] colArray = new int[9];
+            for(int row = 0; row<9;row++){
+                colArray[row] = board[row][column];
+            }
+            if(hasDuplicates(colArray)){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean validateAllBoxes(int[][] board){
+        for(int boxRow = 0; boxRow < 3; boxRow++){
+            for(int boxCol = 0; boxCol < 3; boxCol++){
+                int[] boxArray = new int[9];
+                int index = 0;
+                for(int row = boxRow * 3; row < boxRow * 3 + 3; row++){
+                    for(int col = boxCol * 3; col < boxCol * 3 + 3; col++){
+                        boxArray[index++] = board[row][col];
+                    }
+                }
+                if(hasDuplicates(boxArray)){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private boolean hasDuplicates(int[] array) {
+        boolean[] seen = new boolean[10];
+
+        for (int value : array) {
+            if (value != 0) {
+                if (seen[value]) {
+                    return true;
+                }
+                seen[value] = true;
+            }
+        }
+        return false;
+    }
+
 }
