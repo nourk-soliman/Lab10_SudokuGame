026de134f98c1afd3abcb65a69a481bbd17af017
nourk-soliman <es.nour.soliman2023@alexu.edu.eg>
2025-12-20 10:36:13 +0200

    added class logic

--- src/system/GameCatalog.java
+++ src/system/GameCatalog.java
@@ -10,4 +10,35 @@ package system;
  */
 public class GameCatalog {
     
+    private boolean current;
+    private boolean allModesExist;
+
+
+    
+    public GameCatalog(boolean current,boolean allModesExist){
+        this.current=current;
+        this.allModesExist=allModesExist;
+    }//this is for the viewer
+
+    public boolean isCurrent() {
+        return current;
+    }
+
+    public void setCurrent(boolean current) {
+        this.current=current;
+    
+    }
+
+    public boolean isAllModesExist() {
+        return allModesExist;
+    }
+
+    public void setAllModesExist(boolean allModesExist) {
+        this.allModesExist=allModesExist;
+
 }
+ 
+ 
+    
+}
+
