5e8a4b585fb223e2f52bcbff6627b7a95a86e545
nourk-soliman <es.nour.soliman2023@alexu.edu.eg>
2025-12-20 21:35:23 +0200

    Add files via upload

---
+++ src/controller/system/LogFileHandler.java
@@ -0,0 +1,63 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controller.system;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Nour
+ */
+public class LogFileHandler {
+    private final String filename="N:\\Term 5\\Programming 2\\SudokuGame\\current game\\incomplete.log";
+    
+    public String readAndRemoveLastMove()  {
+        ArrayList<String> lines = new ArrayList<>();
+        try{BufferedReader br = new BufferedReader(new FileReader(filename)); 
+            String line;
+            while ((line = br.readLine()) != null) {
+                lines.add(line);
+            }
+        }
+        catch(IOException e)
+        {System.out.println("File not found.");}
+
+        if (lines.isEmpty()) {
+            return null; // nothing to undo
+        }
+
+        String lastLine = lines.get(lines.size() - 1);
+        lines.remove(lines.size()-1);
+         try {BufferedWriter bw = new BufferedWriter(new FileWriter(filename));
+            for (String l : lines) {
+                bw.write(l);
+                bw.newLine();
+            }
+         }
+         catch(IOException e)
+         {System.out.println("Could not write to file.");}
+         
+        return lastLine;
+    }
+    
+    public void writeToFile(String userAction){
+        try{
+        BufferedWriter writer=new BufferedWriter(new FileWriter(filename,true));
+        writer.write(userAction);
+        writer.newLine();
+        writer.close();
+        }
+        catch(IOException e){
+            System.out.println("Error in writing file");
+        }
+        
+    }
+    
+}
