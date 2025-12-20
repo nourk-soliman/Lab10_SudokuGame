b8263af550f7a47d9327d9299ad60aa0615284a9
nourk-soliman <es.nour.soliman2023@alexu.edu.eg>
2025-12-20 20:18:11 +0200

    Add files via upload

--- src/view/model/UserAction.java
+++ src/view/model/UserAction.java
@@ -9,5 +9,16 @@ package view.model;
  * @author Nour
  */
 public class UserAction {
+    private int row;
+    private int colomn;
+    private int value;
+    private int previous;
+
+    public UserAction(int row, int colomn, int value, int previous) {
+        this.row = row;
+        this.colomn = colomn;
+        this.value = value;
+        this.previous = previous;
+    }
     
 }
