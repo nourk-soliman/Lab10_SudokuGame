2c9bd1b9e19ce9d45f80d74c8fb6bcc7d63fb9e4
Ali1791 <alitarek13062005@gmail.com>
2025-12-20 12:56:06 +0200

    Refactor CSVReader class with proper package declaration

--- src/controller/csv/CSVGameHandler.java
+++ src/controller/csv/CSVGameHandler.java
@@ -44,10 +44,11 @@ public class CSVReader {
             for (int r = 0; r < 9; r++) {
                 for (int c = 0; c < 9; c++) {
                     writer.write(String.valueOf(board[r][c]));
-                    if (c < 8) writer.write(","); // فواصل بين الأعمدة
+                    if (c < 8) writer.write(","); 
                 }
                 writer.newLine();
             }
         }
     }
 }
+
