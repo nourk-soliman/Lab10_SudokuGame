2007e253225874601b2d0098f1b33ba94345cb81
nourk-soliman <es.nour.soliman2023@alexu.edu.eg>
2025-12-20 10:33:53 +0200

    added Controller logic

--- src/interfaces/Controllable.java
+++ src/interfaces/Controllable.java
@@ -3,11 +3,23 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
  */
 package interfaces;
+import exceptions.*;
+import java.io.IOException;
+
 
 /**
  *
  * @author Nour
  */
 public interface Controllable {
-    
+   boolean[] getCatalog();
+  int[][] getGame(char level) throws NotFoundException;
+  void driveGames(String sourcePath) throws SolutionInvalidException;
+  // A boolean array which says if a specifc cell is correct or invalid
+  boolean[][] verifyGame(int[][] game);
+  // contains the cell x, y and solution for each missing cell
+  int[][] solveGame(int[][] game) throws InvalidGame;
+  // Logs the user action
+  void logUserAction(UserAction userAction) throws IOException;
 }
+
