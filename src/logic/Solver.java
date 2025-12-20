fcf9a1c19df4f746c7fd3b90f79b756e7b928a74
Ali1791 <alitarek13062005@gmail.com>
2025-12-20 12:04:07 +0200

    Update Solver.java

--- src/logic/Solver.java
+++ src/logic/Solver.java
@@ -1,22 +1,52 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package logic;
 
 import board.SudokuBoard;
+import exceptions.SolutionInvalidException;
 
-/**
- *
- * @author Ali
- */
 public class Solver {
 
     public Solver() {
     }
-    public static boolean solve(int[][] board){
-    SudokuBoard b=new SudokuBoard();
-    int[][] empties=b.findEmptyCells(board);
+
+    public static boolean solve(int[][] board) {
+
+        // Get the 5 empty cell positions as Flyweight objects
+        SudokuBoard sudokuBoard = new SudokuBoard();
+        CellFlyweight[] emptyCells = sudokuBoard.findEmptyCells(board);
+
+        PermutationIterator iterator = new PermutationIterator();
+        SudokuValidator validator = new SudokuValidator();
+
+        while (iterator.hasNext()) {
+
+            int[] values = iterator.next();
+
+            // Fill the empty cells using Flyweight positions
+            for (int i = 0; i < 5; i++) {
+                int row = emptyCells[i].getRow();
+                int col = emptyCells[i].getCol();
+                board[row][col] = values[i];
+            }
+
+            // Validate the full board
+            try {
+                validator.validateSourceSolution(board);
+                // Valid solution found
+                return true;
+
+            } catch (SolutionInvalidException e) {
+                // Invalid permutation, continue
+            }
+
+            // Reset cells to 0
+            for (int i = 0; i < 5; i++) {
+                int row = emptyCells[i].getRow();
+                int col = emptyCells[i].getCol();
+                board[row][col] = 0;
+            }
+        }
+
+        // No valid solution found
         return false;
     }
 }
