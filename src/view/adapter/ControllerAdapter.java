f56a581c4a84b7662ceba7b4d9c3f84dca3f6f07
nourk-soliman <es.nour.soliman2023@alexu.edu.eg>
2025-12-21 12:15:23 +0200

    Update ControllerAdapter.java
    added solveGame and verifyGame logic

--- src/view/adapter/ControllerAdapter.java
+++ src/view/adapter/ControllerAdapter.java
@@ -9,13 +9,12 @@ import controller.Viewable;
 import controller.exceptions.InvalidGame;
 import controller.exceptions.NotFoundException;
 import controller.exceptions.SolutionInvalidException;
-import controller.logic.CellFlyweight;
-import controller.logic.Solver;
 import controller.model.DifficultyEnum;
 import controller.model.Game;
 import controller.system.GameCatalog;
 import controller.system.GameStorage;
 import java.io.IOException;
+import java.util.ArrayList;
 import view.model.UserAction;
 
 /**
@@ -46,14 +45,13 @@ public class ControllerAdapter implements Controllable {
 
     @Override
     public int[][] getGame(char level) throws NotFoundException {
-        DifficultyEnum difficultyChoice = null;
         switch (level){
             case 'e':
-              return controller.getGame(difficultyChoice.EASY).getBoard(); 
+              return controller.getGame(DifficultyEnum.EASY).getBoard(); 
             case 'm':
-            controller.getGame(difficultyChoice.MEDIUM).getBoard();     
+            controller.getGame(DifficultyEnum.MEDIUM).getBoard();     
             case'h':
-                controller.getGame(difficultyChoice.HARD).getBoard();
+                controller.getGame(DifficultyEnum.HARD).getBoard();
             default:
                 return null;
         }
@@ -73,45 +71,70 @@ public class ControllerAdapter implements Controllable {
 
     @Override
     public boolean[][] verifyGame(int[][] game) {
-         /* GameStorage gameStorage=new GameStorage();
+        if(game==null) return null;
          Game newGame=new Game(game);
-         controller.driveGames(newGame);*/return null;
-         /* GameStorage gameStorage=new GameStorage();
-         Game newGame=new Game(game);
-         controller.driveGames(newGame);*/
+         boolean cellState[][]=new boolean[9][9];
+         String boardState=controller.verifyGame(newGame);
+        
+          for(int i=0;i<9;i++)//initialise boolean array with all true.
+         {for(int j=0;j<9;j++)
+             cellState[i][j]=true;}
+          
+         if(boardState.equalsIgnoreCase("Valid".trim())|| boardState.equalsIgnoreCase("Incomplete".trim())){
+             //return as all true, even if incomplete and valid.
+         return cellState;}
+         
+         if(boardState.equalsIgnoreCase("Invalid".trim())){
+             String [] invalidCells=boardState.split(" ");//to split each location.
+             
+             for(int i=1;i<invalidCells.length;i++)//we started from 1 to ignore the "invalid" word
+             {String[]invalidCellPosition=invalidCells[i].split(",");//to split the row and coloumn
+             int row=Integer.parseInt(invalidCellPosition[0]);
+             int colomn=Integer.parseInt(invalidCellPosition[1]);
+             cellState[row][colomn]=false;} 
+             
+             return cellState;
+         }
+         return null;
+        
     }
+@Override
+public int[][] solveGame(int[][] game) throws InvalidGame {
 
-    @Override
-    public int[][] solveGame(int[][] game) throws InvalidGame {
-   if (game == null) {
+    if (game == null) {
         throw new InvalidGame("The game board is null");
     }
 
-   
-    Game tempGame = new Game(game);
+    // Find empty cells before solving,if they are are not equal to 5, then the game will not be solved.
+    ArrayList<int[]> emptyCells = new ArrayList<>();//ArrayList of an array of integers because we need to store the row and colomn of the empty cell.
 
-   
-    CellFlyweight[] emptyCells = new Game(game).findEmptyCells(game);
-
-   
-    boolean solved = Solver.solve(game);
-
-    if (!solved) {
-        throw new InvalidGame("No valid solution found for the game");
+    for (int row = 0; row < 9; row++) {
+        for (int col = 0; col < 9; col++) {
+            if (game[row][col] == 0) {
+                emptyCells.add(new int[]{row, col});
+            }
+        }
     }
 
-  
-    int[][] solvedCells = new int[emptyCells.length][3]; // row, col, value
-    for (int i = 0; i < emptyCells.length; i++) {
-        int row = emptyCells[i].getRow();
-        int col = emptyCells[i].getCol();
-        solvedCells[i][0] = row;
-        solvedCells[i][1] = col;
-        solvedCells[i][2] = game[row][col]; 
+    if (emptyCells.size() != 5) {
+        throw new InvalidGame("Solve allowed only when exactly 5 cells are empty");
+    }
+
+    // adapter pattern here.
+    Game controllerGame = new Game(game);
+    int[] solvedValues = controller.solveGame(controllerGame);
+
+    int[][] solvedCells = new int[solvedValues.length][3];
+
+    for (int i = 0; i < solvedValues.length; i++) {
+        solvedCells[i][0] = emptyCells.get(i)[0]; // row
+        solvedCells[i][1] = emptyCells.get(i)[1]; // colomn
+        solvedCells[i][2] = solvedValues[i];      // value
     }
 
     return solvedCells;
-    }
+}
+
 
     @Override
     public void logUserAction(UserAction userAction) throws IOException {
